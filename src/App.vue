22826db5c5e668030e3414314eb115f113ecadb0
Gisela <gmontaneregea@gmail.com>
2026-09-17 18:29:20 +0200

    bases-ts-vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+//import ControlFlujo from './typescript/ControlFlujo.vue';
+
+
+
+
+</script>
+
+
+<template>
+
+  <h1>Vue + TS</h1>
+  
+ <!-- <ConstLet/> -->
+ <!--<ControlFlujo/>-->
+
+</template>
+
+
+
---
+++ src/typescript/ConstLet.vue
@@ -0,0 +1,35 @@
+
+<script setup lang="ts">
+
+let urbs: string = 'Valencia'
+urbs = 'Madrid'
+
+//console.log(urbs);
+
+const aetas: number = 24
+
+const nomen =  'Tony'
+//nomen =  'fernando'
+
+const cognomen =  'Stack'
+
+const plenumNomen = `${ nomen } ${ cognomen }`
+
+console.log(`${plenumNomen.toUpperCase()} es de ${urbs} y tiene ${aetas + 3} años`)
+
+
+</script>
+
+
+<template>
+    <div>
+        <h2>Tipos básicos</h2>
+
+        <p>{{ plenumNomen.toLocaleUpperCase ()}} es de {{ urbs }} y tiene {{ aetas + 3}} años</p>
+    </div>
+</template>
+
+
+<style scoped> 
+
+</style>
